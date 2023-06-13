ac80f9116acfccb48a37eccf33c25f29f7162224
OleksandrChaika13 <axelrod1347@gmail.com>
2023-06-13 11:23:56 +0300

    Update app information

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app. Super!");
+        System.out.println("Calculator app.");
         System.out.println("Version 1.0");
     }
 }
